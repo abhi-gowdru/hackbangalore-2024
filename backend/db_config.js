2f75ec30fb64856da3515406663efb8b4d0699cd
Abhilash Gowda P <abhilashgowda14012014@gmail.com>
2024-05-11 18:54:32 +0530

    Database configured of Firebase

---
+++ backend/db_config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDO-CTaRBzSjCR4C5nnluMP6nHeit5FM1M",
+  authDomain: "hack-bangalore-2024.firebaseapp.com",
+  projectId: "hack-bangalore-2024",
+  storageBucket: "hack-bangalore-2024.appspot.com",
+  messagingSenderId: "403908037976",
+  appId: "1:403908037976:web:26e1d0b4e9c288fc6fa92d",
+  measurementId: "G-6271NR4JX2"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize services
+const auth = getAuth(app);
+
+export default auth;
